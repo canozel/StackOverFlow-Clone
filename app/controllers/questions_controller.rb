b385bf6d71fdc14eecdaef08d92bb2b1a5cc8ee0
Salih Can ÖZEL <can.ozel@bil.omu.edu.tr>
2015-08-04 12:37:21 +0300

    devise kurduk

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,10 +15,11 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(question_params)
+    @question.user = current_user
     if @question.save
       redirect_to root_path, notice: "Kayıt Başarılı"
     else
-      render :new 
+      render :new, alert: "Kayıt işlemi yapılırken bir hata ile karşılaşıldı."
     end
   end
 
