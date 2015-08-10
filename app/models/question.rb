e6fed15fc9ef9a2fac3ea7b3f7f24a2d6a860391
Salih Can ÖZEL <can.ozel@bil.omu.edu.tr>
2015-08-10 14:08:48 +0300

    Site ismi değiştirildi

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   validates :title, :content, presence: true #Boş geçilmesin
-  validates :title, length: {maximum: 20}
+  validates :title, length: {min: 3}
 
   has_many :answers, dependent: :destroy
   belongs_to :user
